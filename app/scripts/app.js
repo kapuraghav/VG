eb548afdb7909faa3ee4376c9f2065f0d4314b42
Raghav Kapoor <kapuraghav@gmail.com>
2013-11-05 08:38:13 -0500

    adding partials for navigation bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,43 @@
 'use strict';
 
 angular.module('vedicApp', ['ui.event'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      }).when('/demo',{
-         templateUrl:'views/Demo/demo.html',
-         controller: 'DemoCtrl'
-        })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Demo/demo.html',
+                controller: 'DemoCtrl'
+            }).when('/AboutScholar',{
+                templateUrl:'views/Partial/AboutScholar.html',
+                controller: 'PartialCtrl'
+            }).when('/BasisOfVedicLiterature',{
+                templateUrl:'views/Partial/BasisOfVedicLiterature.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            }).when('/demo',{
+                templateUrl:'views/Partial/demo.html',
+                controller: 'PartialCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
