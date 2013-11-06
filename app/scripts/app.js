e2b0e6b49dc1d7e2e412590caabe2d2402dab0d4
Raghav Kapoor <kapuraghav@gmail.com>
2013-11-06 09:01:47 -0500

    added routes to the different pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,26 +15,35 @@ angular.module('vedicApp', ['ui.event'])
             }).when('/BasisOfVedicLiterature',{
                 templateUrl:'views/Partial/BasisOfVedicLiterature.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/feedback',{
+                templateUrl:'views/Partial/feedback.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/fermats',{
+                templateUrl:'views/Partial/Fermats.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/ganitsutras',{
+                templateUrl:'views/Partial/ganitsutras.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/glimpses',{
+                templateUrl:'views/Partial/glimpses.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/goldbachs',{
+                templateUrl:'views/Partial/goldbachs.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/publishedbooks',{
+                templateUrl:'views/Partial/publishedbooks.html',
                 controller: 'PartialCtrl'
-            }).when('/demo',{
-                templateUrl:'views/Partial/demo.html',
+            }).when('/scriptures',{
+                templateUrl:'views/Partial/scriptures.html',
+                controller: 'PartialCtrl'
+            }).when('/timeandspace',{
+                templateUrl:'views/Partial/timeandspace.html',
+                controller: 'PartialCtrl'
+            }).when('/vedicmathematicslinks',{
+                templateUrl:'views/Partial/vedicmathematicslinks.html',
+                controller: 'PartialCtrl'
+            }).when('/vmanswers',{
+                templateUrl:'views/Partial/vmanswers.html',
                 controller: 'PartialCtrl'
             })
             .otherwise({
